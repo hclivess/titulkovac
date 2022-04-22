6d0bd6195c060ae0167367fda36059913343c866
Jan Kučera <admin@bismuth.cz>
2022-04-23 01:09:01 +0200

    Update titulkovac-turbo.py

--- titulkovac-turbo.py
+++ titulkovac-turbo.py
@@ -1,9 +1,10 @@
-#kompilace: python -m nuitka --follow-imports titulkovac.py --standalone --show-progress -j 8 --recurse-all
+# kompilace: python -m nuitka --follow-imports titulkovac.py --standalone --show-progress -j 8 --recurse-all
 
 
 import glob
 import os
 import time
+import re
 
 """
 subfolder = "opraveno"
@@ -12,7 +13,13 @@ if not os.path.exists(subfolder):
     os.mkdir(subfolder)
 """
 
-types = ('**/*.sub', '**/*.srt', '*.srt') # the tuple of file types
+
+def multiple_replace(string, rep_dict):
+    pattern = re.compile("|".join([re.escape(k) for k in sorted(rep_dict, key=len, reverse=True)]), flags=re.DOTALL)
+    return pattern.sub(lambda x: rep_dict[x.group(0)], string)
+
+
+types = ('**/*.sub', '**/*.srt', '*.srt')  # the tuple of file types
 files_grabbed = []
 for files in types:
     files_grabbed.extend(glob.glob(files))
@@ -28,41 +35,21 @@ for file in files_grabbed:
                 file_contents_orig = file_open.read()
                 file_contents = file_contents_orig
 
-                file_contents = file_contents.replace("ø","ř")
-                file_contents = file_contents.replace("Ø", "ř")
-                file_contents = file_contents.replace("", "š")
-                file_contents = file_contents.replace("è", "č")
-                file_contents = file_contents.replace("È", "Č")
-                file_contents = file_contents.replace("ï", "ď")
-                file_contents = file_contents.replace("Ï", "Ď")
-                file_contents = file_contents.replace("", "ž")
-                file_contents = file_contents.replace("", "Ž")
-                file_contents = file_contents.replace("ì", "ě")
-                file_contents = file_contents.replace("Ì", "Ě")
-                file_contents = file_contents.replace("ù", "ů")
-                file_contents = file_contents.replace("Ù", "Ů")
-                file_contents = file_contents.replace("", "'")
-                file_contents = file_contents.replace("", "'")
-                file_contents = file_contents.replace("", "ť")
-                file_contents = file_contents.replace("ò", "ň")
-                file_contents = file_contents.replace("Ò", "Ň")
+                file_contents = multiple_replace(file_contents,
+                                                 {"ø": "ř", "": "š", "è": "č", "È": "Č", "ï": "ď", "Ï": "Ď", "": "ž",
+                                                  "": "Ž", "ì": "ě", "Ì": "Ě", "ù": "ů", "Ù": "Ů", "": "'", "": "'",
+                                                  "": "ť", "ò": "ň", "Ò": "Ň", "Ø": "ř"})
 
-                #print(file_contents)
+                # print(file_contents)
 
                 with open(file, "w", encoding="utf-8") as fo:
                     print(f"Opened {file} because it is {encoding_var}")
                     fo.write(file_contents)
 
                     if b'file_contents_orig' != b'file_contents':
-                        print(f"Finished {file}, difference of {abs(len(b'file_contents_orig')-len(b'file_contents'))}")
+                        print(
+                            f"Finished {file}, difference of {abs(len(b'file_contents_orig') - len(b'file_contents'))}")
 
         except Exception as e:
             if not type(e) is UnicodeDecodeError:
                 print(f"Exception with {file}: {e}")
-
-
-
-
-
-
-
