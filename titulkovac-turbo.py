37a3b5435bd7c0cfa69e725077138bacb2205925
Jan Kučera <admin@bismuth.cz>
2022-04-22 23:32:29 +0200

    Update titulkovac-turbo.py

--- titulkovac-turbo.py
+++ titulkovac-turbo.py
@@ -25,34 +25,36 @@ for file in files_grabbed:
     for encoding_var in encodings:
         try:
             with open(file, "r+", encoding=encoding_var) as file_open:
-                file_contents = file_open.read()
+                file_contents_orig = file_open.read()
+                file_contents = file_contents_orig
 
-                file_contents_new = file_contents.replace("ø","ř")
-                file_contents_new = file_contents.replace("Ø", "ř")
-                file_contents_new = file_contents.replace("", "š")
-                file_contents_new = file_contents.replace("è", "č")
-                file_contents_new = file_contents.replace("È", "Č")
-                file_contents_new = file_contents.replace("ï", "ď")
-                file_contents_new = file_contents.replace("Ï", "Ď")
-                file_contents_new = file_contents.replace("", "ž")
-                file_contents_new = file_contents.replace("", "Ž")
-                file_contents_new = file_contents.replace("ì", "ě")
-                file_contents_new = file_contents.replace("Ì", "Ě")
-                file_contents_new = file_contents.replace("ù", "ů")
-                file_contents_new = file_contents.replace("Ù", "Ů")
-                file_contents_new = file_contents.replace("", "'")
-                file_contents_new = file_contents.replace("", "'")
-                file_contents_new = file_contents.replace("", "ť")
-                file_contents_new = file_contents.replace("ò", "ň")
-                file_contents_new = file_contents.replace("Ò", "Ň")
+                file_contents = file_contents.replace("ø","ř")
+                file_contents = file_contents.replace("Ø", "ř")
+                file_contents = file_contents.replace("", "š")
+                file_contents = file_contents.replace("è", "č")
+                file_contents = file_contents.replace("È", "Č")
+                file_contents = file_contents.replace("ï", "ď")
+                file_contents = file_contents.replace("Ï", "Ď")
+                file_contents = file_contents.replace("", "ž")
+                file_contents = file_contents.replace("", "Ž")
+                file_contents = file_contents.replace("ì", "ě")
+                file_contents = file_contents.replace("Ì", "Ě")
+                file_contents = file_contents.replace("ù", "ů")
+                file_contents = file_contents.replace("Ù", "Ů")
+                file_contents = file_contents.replace("", "'")
+                file_contents = file_contents.replace("", "'")
+                file_contents = file_contents.replace("", "ť")
+                file_contents = file_contents.replace("ò", "ň")
+                file_contents = file_contents.replace("Ò", "Ň")
 
                 #print(file_contents)
 
                 with open(file, "w", encoding="utf-8") as fo:
-                    fo.write(file_contents_new)
+                    print(f"Opened {file} because it is {encoding_var}")
+                    fo.write(file_contents)
 
-                    if file_contents != file_contents_new:
-                        print(f"Finished {file}, difference of {abs(len(file_contents_new)-len(file_contents))}")
+                    if b'file_contents_orig' != b'file_contents':
+                        print(f"Finished {file}, difference of {abs(len(b'file_contents_orig')-len(b'file_contents'))}")
 
         except Exception as e:
             if not type(e) is UnicodeDecodeError:
